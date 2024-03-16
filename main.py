c8c24b241a83d9731c0a60bdf96015d52eaf5e0d
“mtsukka2727” <tsukasa.tukka3710@icloud.com>
2024-03-16 20:20:13 +0900

    メモ保存機能追加

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import streamlit as st
 
-st.title("tskka moji count")
-input_text = st.text_area('asdf')
+st.title("tsukka文字数カウントアプリ")
+
+if 'tsukka_memo' not in st.session_state:
+    st.session_state['tsukka_memo'] =list()
+
+input_text = st.text_area('文字を入力してください')
 
 is_pushed = st.button("文字カウント")
 
 if is_pushed:
-    st.text(len(input_text))
+    st.text(f"{len(input_text)}字")
 else:
     pass
+
+if st.button("保存する"):
+    st.session_state['tsukka_memo'].append(input_text)
+
+
+for _ in st.session_state['tsukka_memo']:
+    st.text(_)
+    st.text(f"{len(_)}字")
+    st.divider()
+    st.button()
